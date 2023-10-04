aba25ba73f3abb1048d589a782cd95aadc0d22f9
Snehal Poptani <poptanisnehal@gmail.com>
2023-10-04 15:47:28 +0530

    commit

--- medidove-react/src/components/Firebase/Firebase.config.js
+++ medidove-react/src/components/Firebase/Firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDCQ67W0QO-9cleICWUA2vYJTj3CLGXVmI",
-    authDomain: "medidove-1e1cd.firebaseapp.com",
-    projectId: "medidove-1e1cd",
-    storageBucket: "medidove-1e1cd.appspot.com",
-    messagingSenderId: "670168320594",
-    appId: "1:670168320594:web:9011b007030d3997beed8b"
-};
+    apiKey: "AIzaSyDj7IHNgEflYB4Cfi6QOrBY68limEfOqIA",
+    authDomain: "fire-crud-b4b7d.firebaseapp.com",
+    projectId: "fire-crud-b4b7d",
+    storageBucket: "fire-crud-b4b7d.appspot.com",
+    messagingSenderId: "1006424836130",
+    appId: "1:1006424836130:web:d5e0fbe3371b148f79227b",
+    measurementId: "G-G7BQ3VWRFW"
+  };
 
 export default firebaseConfig;
--- medidove-react/src/components/Firebase/Firebase.init.js
+++ medidove-react/src/components/Firebase/Firebase.init.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./Firebase.config";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseInitialization = () => {
     initializeApp(firebaseConfig);
